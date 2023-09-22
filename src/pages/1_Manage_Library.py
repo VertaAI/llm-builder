93915bd90e9ad68fb11cc62054fcefb83a00dc59
John K. Watson <john@verta.ai>
2023-09-22 08:53:26 -0700

    use aggrid to enable multi-line editable table

--- src/pages/1_Manage_Library.py
+++ src/pages/1_Manage_Library.py
@@ -1,22 +1,26 @@
+import pandas as pd
 import streamlit as st
 
 import table
+from st_aggrid import AgGrid, ColumnsAutoSizeMode, ExcelExportMode, GridOptionsBuilder
+
 models = st.session_state['models']
 
 (datasets, prompts) = table.load_data()
 df = table.create_table(datasets, models, prompts, cached=True)
 df_with_selections = df.copy()
 df_with_selections.insert(0, "Select", False)
-edited_df = st.data_editor(
-    df_with_selections,
-    hide_index=True,
-    column_config={"Select": st.column_config.CheckboxColumn(required=True)},
-    disabled=df.columns,
-    width=10000,
-)
 
-selected_rows = edited_df[edited_df.Select]
-selection = selected_rows.drop('Select', axis=1)
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, wrapText=True, autoHeight=True)
+gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+library_grid = AgGrid(df, height=500, columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW, gridOptions=gb.build(), key='grid')
+
+selected_rows = library_grid["selected_rows"]
+selection = pd.DataFrame(selected_rows)
+# TODO: this isn't working as expected. The column is not being dropped.
+if '_selectedRowNodeInfo' in selection.columns:
+    selection.drop(labels='_selectedRowNodeInfo', axis=1)
 st.write("Selected rows:")
 st.data_editor(
     selection,
