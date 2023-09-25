ddfeca0512e03a001bb0d60f100e26da6b65ff7b
John K. Watson <john@verta.ai>
2023-09-25 08:15:19 -0700

    Add the ability to edit and save an existing prompt

--- src/pages/2_Refine_Prompts.py
+++ src/pages/2_Refine_Prompts.py
@@ -22,12 +22,19 @@ gb = GridOptionsBuilder.from_dataframe(prompt_frame)
 gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, autoHeight=True, wrapText=True)
 gb.configure_selection(selection_mode="single", use_checkbox=False)
 gb.configure_column("id", hide=True)
-samples_grid = AgGrid(
+prompt_grid = AgGrid(
     prompt_frame,
     data_return_mode=DataReturnMode.AS_INPUT,
     update_mode=GridUpdateMode.SELECTION_CHANGED,
     columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW,
     gridOptions=gb.build(), height=500)
+
+selected = prompt_grid["selected_rows"]
+prompt = None
+if len(selected) == 1:
+    st.subheader('Update prompt')
+    prompt = selected[0]
+# st.write(prompt)
 
 st.subheader('Prompt playground')
 with st.form("playground", clear_on_submit=False):
@@ -39,7 +46,11 @@ with st.form("playground", clear_on_submit=False):
 
     if st.session_state.pop('prompt_content'+_FORM_VALIDATION_KEY, None):
         st.error('Prompt content cannot be empty')
-    prompt_content = st.text_area('Enter a prompt:', height=200, key='prompt_content')
+
+    if prompt:
+        prompt_content = st.text_area('Enter a prompt:', height=200, key='prompt_content', value=prompt['prompt'])
+    else:
+        prompt_content = st.text_area('Enter a prompt:', height=200, key='prompt_content')
 
     f1, f2 = st.columns([0.1, 0.9])
     with f1:
@@ -62,7 +73,10 @@ with st.form("playground", clear_on_submit=False):
         if len(prompt_content) > 0:
             model = [m for m in models if m.get_name() == model_selection][0]
             dataset = [d for d in datasets if d.name == dataset_selection][0]
-            prompt_id = len(prompts)
+            if prompt:
+                prompt_id = prompt['id']
+            else:
+                prompt_id = len(prompts)
             prompt = Prompt(prompt_id, prompt_content)
             prompt.save()
             run_computations(model, prompt, dataset)
